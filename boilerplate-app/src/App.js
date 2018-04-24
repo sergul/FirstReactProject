ce50266aadaadab21d1fdcde5590da0af5025125
Sergey <serguljan@gmail.com>
2018-04-23 18:45:06 -0700

    Added a React Boilerplate app

---
+++ boilerplate-app/src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>React Boilerdf
+            </div>
+        );
+    }
+}
+
+export default App;
