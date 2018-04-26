fb4486d8bff4e5edcd54a659b5eac946b61e1266
Sergey <serguljan@gmail.com>
2018-04-25 20:16:56 -0700

    Refactored to custom-react-boilerplate-app

---
+++ custom-react-boilerplate-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+const rootJSX = (<App/>);
+
+ReactDOM.render(rootJSX, document.getElementById('root'));
