143b3e20e4ec67cacd95bc48b0f0bd710669febe
Sergey <serguljan@gmail.com>
2019-05-06 00:56:39 -0700

    Refactored bio text.

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -16,7 +16,7 @@ class Header extends PureComponent {
             I'm a front-end developer mainly specialized in UI development technologies for complex scalable web app development.<br/>
             I also have experience in developing ASP.NET Core based middle-tier backend services. I worked as a front-end developer in<br/>
             <a className="link-on-header" target="_blank" href="https://www.hcltech.com/">HCL America</a> and currently working as a Senior web/app developer in <a className="link-on-header" target="_blank" href="https://en.pactera.com/">Pactera Technologies</a><br/>
-            You can find more about my career journey in my <Link className="link-on-header" to="/Resume">resume.</Link>
+            You can find more about my career journey in <Link className="link-on-header" to="/Resume">resume.</Link>
           </div>
         </div>
       </div>
