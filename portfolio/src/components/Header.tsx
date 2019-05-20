c2f183bf8022affe2a12bbcec4f0cd8232cddec7
Sergey <serguljan@gmail.com>
2019-05-20 00:47:09 -0700

    changed styles

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -8,24 +8,28 @@ import NavBarItem from './reusable/nav-bar/NavBarItem';
 class Header extends PureComponent {
   render() {
     return (
-      <div className="header-container padding-top-normal">
-          <NavBar >
+      <div className="header-container">
+          <NavBar>
             <NavBarItem key="0" label="home" route="/"/>
             <NavBarItem key="1" label="resume" route="/resume"/>
           </NavBar>
-          {/* labels={[{label: 'About', route: '/'}, {label: 'Resume', route: '/resume'}]} */}
-          <img className="avatar" alt="Me :)" src={getImage('avatar.jpg')}></img>
-          <div className="header-info-container">
-            <div className="medium-font-size">Hi :) My name is:</div>
-            <h2 className="large-font-size">Sergey Mikayelyan</h2>
-            <div className="small-font-size padding-top-normal">
-              I'm a passionate Software Developer who've been intensively developing and delivering<br/>
-              software products and services more than 9 years. During this beautiful journey I've<br/>
-              always put my first priority on giving <b>quality</b> and client satisfaction in work / end result.<br/>
-              Throughout my career path I've always sharpened my professional skills as I've been<br/>
-              in Game, Desktop and Web development. You can find more about experience in my <Link className="link-on-header" to="/resume">resume</Link> :)
+          <div className="vertically-center">
+              <div className="header-avatar__container inline-block">
+                <img className="header-avatar" alt="Me :)" src={getImage('avatar.jpg')}></img>
+              </div>
+              <div className="header-bio-container inline-block">
+                <p className="medium-font-size">Hi :) My name is:</p>
+                <h2 className="large-font-size">Sergey Mikayelyan</h2>
+                <p className="small-font-size padding-top-normal">
+                  I'm a passionate Software Developer who've been intensively developing and delivering<br/>
+                  software products and services more than 9 years. During this beautiful journey I've<br/>
+                  always put my first priority on giving <b>quality</b> and client satisfaction in work / end result.<br/>
+                  Throughout my career path I've always sharpened my professional skills as I've been<br/>
+                  in Game, Desktop and Web development. You can find more about experience in my <Link className="link-on-header" to="/resume">resume</Link> :)
+                </p>
+              </div>
+              
             </div>
-          </div>
         </div>
       
     );
