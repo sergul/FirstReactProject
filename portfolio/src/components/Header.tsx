fe6b6ab9e7d7e7f01584c2c3e8186e19a0d15e7e
Sergey Mikayelyan <serguljan@gmail.com>
2019-05-20 16:54:04 -0700

    Fixed layouts in header section

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -9,29 +9,29 @@ class Header extends PureComponent {
   render() {
     return (
       <div className="header-container">
-          <NavBar>
-            <NavBarItem key="0" label="home" route="/"/>
-            <NavBarItem key="1" label="resume" route="/resume"/>
-          </NavBar>
-          <div className="vertically-center">
-              <div className="header-avatar__container inline-block">
-                <img className="header-avatar" alt="Me :)" src={getImage('avatar.jpg')}></img>
-              </div>
-              <div className="header-bio-container inline-block">
-                <p className="medium-font-size">Hi :) My name is:</p>
-                <h2 className="large-font-size">Sergey Mikayelyan</h2>
-                <p className="small-font-size padding-top-normal">
-                  I'm a passionate Software Developer who've been intensively developing and delivering<br/>
-                  software products and services more than 9 years. During this beautiful journey I've<br/>
-                  always put my first priority on giving <b>quality</b> and client satisfaction in work / end result.<br/>
-                  Throughout my career path I've always sharpened my professional skills as I've been<br/>
-                  in Game, Desktop and Web development. You can find more about experience in my <Link className="link-on-header" to="/resume">resume</Link> :)
+        <NavBar>
+          <NavBarItem key="0" label="home" route="/" />
+          <NavBarItem key="1" label="resume" route="/resume" />
+        </NavBar>
+        <div className="header-center-container">
+          <div className="header-avatar-container inline-block">
+            <img className="header-avatar" alt="Me :)" src={getImage('avatar.jpg')}></img>
+          </div>
+          <div className="header-bio-container inline-block">
+            <p className="medium-font-size">Hi :) My name is:</p>
+            <h2 className="large-font-size">Sergey Mikayelyan</h2>
+            <p className="small-font-size padding-top-normal">
+              I'm a passionate Software Developer who've been intensively developing and delivering<br />
+              software products and services more than 9 years. During this beautiful journey I've<br />
+              always put my first priority on giving <b>quality</b> and client satisfaction in work / end result.<br />
+              Throughout my career path I've always sharpened my professional skills as I've been<br />
+              in Game, Desktop and Web development. You can find more about experience in my <Link className="link-on-header" to="/resume">resume</Link> :)
                 </p>
-              </div>
-              
-            </div>
+          </div>
+
         </div>
-      
+      </div>
+
     );
   }
 }
--- portfolio/src/components/reusable/nav-bar/NavBar.tsx
+++ portfolio/src/components/reusable/nav-bar/NavBar.tsx
@@ -5,20 +5,20 @@ export interface InjectedCounterProps {
 }
 
 class NavBar extends PureComponent<any, any> {
-  constructor(props:any) {
+  constructor(props: any) {
     super(props);
     console.log(props);
   }
 
   render() {
     return (
-      <div className="nav-bar__container">
+      <nav className="nav-bar__container">
         <ul className="nav-bar__list">
           {
             this.props.children
           }
         </ul>
-      </div>
+      </nav>
     );
   }
 }
