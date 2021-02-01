399394795b5310b8b1c2c37ac587d0731fb8248c
Sergey <serguljan@gmail.com>
2021-01-31 23:27:09 -0800

    Fixing heroku

--- app.ts
+++ app.ts
@@ -1,18 +1,16 @@
-#!/usr/bin/env node
+import createError from "http-errors"
+import express from "express";
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-/**
- * Module dependencies.
- */
-
+var usersRouter = require("./routes/problems");
 import { HttpError } from "http-errors";
 
-import app from "../app";
 import debug from "debug";
 import http from "http";
 
-/**
- * Get port from environment and store in Express.
- */
+const app = express();
 
 var port = normalizePort(process.env.PORT || '8080');
 app.set('port', port);
@@ -93,3 +91,40 @@ function onListening() {
     : 'port ' + addr.port;
   debug('Listening on ' + bind);
 }
+
+
+const isProd = (process.env.NODE_ENV || "development") === "production";
+const indexPath = isProd ? "../client/build" : "client/build";
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, indexPath)));
+
+app.use("/api/problems", usersRouter);
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, indexPath, "index.html"));
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err: any, req: any, res: any) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+export default app;
