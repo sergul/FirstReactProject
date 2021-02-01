184282dc60a39661a94479cc5630b665e12fb178
Sergey <serguljan@gmail.com>
2021-01-31 19:39:04 -0800

    Heroku deployment fix

--- app.ts
+++ app.ts
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 app.use('/api/problems', usersRouter);
