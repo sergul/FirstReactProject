e8fa97aa4fa206eda84949e1010cb91b68d30952
Sergey <serguljan@gmail.com>
2021-01-31 20:10:16 -0800

    Refactor

--- app.ts
+++ app.ts
@@ -1,44 +1,44 @@
 import createError from "http-errors"
 import express from "express";
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/problems');
+var usersRouter = require("./routes/problems");
 
 const app = express();
-
+const isProd = (process.env.NODE_ENV || "development") === "production";
+const indexPath = isProd ? "../client/build" : "client/build";
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, indexPath)));
 
-app.use('/api/problems', usersRouter);
+app.use("/api/problems", usersRouter);
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, indexPath, "index.html"));
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err:any, req:any, res:any) {
+app.use(function (err: any, req: any, res: any) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 export default app;
