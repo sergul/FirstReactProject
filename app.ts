a0201b5486d5b430e4caa5a84599aba63ef65b58
Sergey <serguljan@gmail.com>
2021-01-31 19:40:48 -0800

    Express server fix for heroku deployment

--- app.ts
+++ app.ts
@@ -19,10 +19,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client/build')));
 
+app.use('/api/problems', usersRouter);
+
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
-app.use('/api/problems', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
